e1a39caaae40025e336c6ecbf953dfb79a1c091d
Maximilien Naveau <maximilien.naveau@gmail.com>
2024-06-13 19:10:34 +0000

    start adding a trajectory buffer in the code

--- agimus_controller/main_hpp_mpc.py
+++ agimus_controller/main_hpp_mpc.py
@@ -3,6 +3,7 @@
 from agimus_controller.mpc import MPC
 from agimus_controller.hpp_interface import HppInterface
 from agimus_controller.plots import MPCPlots
+from agimus_controller.ocp_croco_hpp import OCPCrocoHPP
 import time
 
 
@@ -12,32 +13,24 @@ if __name__ == "__main__":
     vf = hpp_interface.vf
     ball_init_pose = [-0.2, 0, 0.02, 0, 0, 0, 1]
     hpp_interface.get_hpp_plan(1e-2, 6)
-    chc = MPC(hpp_interface.x_plan, hpp_interface.a_plan, "ur5")
+    ocp = OCPCrocoHPP("ur5")
+    mpc = MPC()
     start = time.time()
-    chc.prob.set_costs(10**4, 1, 10**-3, 0, 0)
-    # chc.search_best_costs(chc.prob.nb_paths - 1, False, False, True)
-    chc.simulate_mpc(100)
+    mpc.initialize(ocp, hpp_interface.x_plan, hpp_interface.a_plan)
+    mpc.prob.set_costs(10**4, 1, 10**-3, 0, 0)
+    # mpc.search_best_costs(mpc.prob.nb_paths - 1, False, False, True)
+    mpc.do_mpc(100)
     end = time.time()
-    u_plan = chc.prob.get_uref(hpp_interface.x_plan, hpp_interface.a_plan)
+    u_plan = mpc.prob.get_uref(hpp_interface.x_plan, hpp_interface.a_plan)
     mpc_plots = MPCPlots(
-        chc.croco_xs,
-        chc.croco_us,
+        mpc.croco_xs,
+        mpc.croco_us,
         hpp_interface.x_plan,
         u_plan,
-        chc.robot,
+        mpc.robot,
         vf,
         ball_init_pose,
-        chc.prob.DT,
+        mpc.prob.DT,
     )
     print("mpc duration ", end - start)
-    """
-    with open("datas.npy", "wb") as f:
-        np.save(f, chc.prob.hpp_paths[0].x_plan)
-        np.save(f, chc.prob.hpp_paths[1].x_plan)"""
-
-"""
-from hpp.gepetto import PathPlayer
-v =vf.createViewer()
-pp = PathPlayer (v)"""
-# plot_costs_from_dic(return_cost_vectors(chc.prob.solver,weighted=True))
-# plot_costs_from_dic(return_cost_vectors(self.prob.solver,weighted=True))
+    mpc_plots.display_path()
